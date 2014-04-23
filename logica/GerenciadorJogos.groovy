ae8153f3ea14c981dad6188d6df1de1a1810d389
Bruno R. Toffoli <bruno.toffoli@geocontrol.com.br>
2014-04-23 17:34:06 -0300

    Melhoria do status do server

--- logica/GerenciadorJogos.groovy
+++ logica/GerenciadorJogos.groovy
@@ -1,5 +1,6 @@
 package logica
 
+import groovy.json.JsonBuilder
 import basico.ObjetoBase
 import dominio.*
 
@@ -9,8 +10,9 @@ class GerenciadorJogos extends ObjetoBase {
     List<Partida> partidas
     List<Convite> convites
 
+
     GerenciadorJogos(int portaServidor) {
-        usuarios = []//.asSynchronized()
+        usuarios = [].asSynchronized()
         partidas = []
         convites = []
 
@@ -35,14 +37,14 @@ class GerenciadorJogos extends ObjetoBase {
                         /*TODO colocar lógica de atualizacao no usuário p/ qualquer ação do mesmo */
                         def reader = input.newReader()
                         def buffer = reader.readLine()
-                        println "servidor recebeu: ${buffer}"
-                        def now = new Date()
-                        output << "echo-response($now): " + buffer + "\n"
+                        processarMensagem(usuario, buffer)
+                        //def now = new Date()
+                        //output << "echo-response($now): " + buffer + "\n"
                     }
-                    println "encerrando conexão socket=$socket e usuario=${usuario}."
+                    
                 }catch (e){
                     println "Erro na conexão do usuário=$usuario socket=$socket"
-
+                    fecharConexao(usuario)
                 }
             }
         }
@@ -50,8 +52,9 @@ class GerenciadorJogos extends ObjetoBase {
 
     void iniciarGerenciaConexoes(){
         /*Iniciando serviço que gerencia conexões*/
-        final long tempoGerenciaConexao = 5000
+        final long tempoGerenciaConexao = 15000
         final long tempoQuedaUsuario = 8000
+        final long tempoDerrubarUsuario = tempoQuedaUsuario * 2
         Thread.startDaemon {
             while(true){
                 sleep(tempoGerenciaConexao)
@@ -59,42 +62,95 @@ class GerenciadorJogos extends ObjetoBase {
                 List<Usuario> usuariosComProblema = usuarios.findAll{Usuario u -> (dataHora.time - u.dtAtualizacao.time) > tempoQuedaUsuario }
 
                 usuariosComProblema.each { Usuario usuario ->
+                    Long tempoMiliSegSemAtualizacao = dataHora.time - usuario.dtAtualizacao.time
                     Socket socket = usuario.socket
-                    if (socket && socket.closed){
-                        usuarios.remove(usuario)
+                    if (tempoMiliSegSemAtualizacao > tempoDerrubarUsuario){
+                        fecharConexao(usuario)
                     } else {
 //                        OutputStream output = socket.outputStream
 //                        output << 'ACK|'
 //                        output.flush()
 //
-                        enviarMensagem()
+                        enviarMensagemReconecao(usuario)
                         /*TODO não é necessário implementar o retorno pois o mesmo já é */
                     }
-                }
-
+                }   
+                printStatus()
             }
 
         }
     }
 
     void processarMensagem(Usuario usuario, String mensagem) {
+        println "recebendo mensagem $usuario: $mensagem"
+
     }
 
 
 
     void enviarMensagem(Usuario usuario, String mensagem) {
-        OutputStream output = socket.outputStream
-        output << mensagem
-        output.flush()
+        try{
+            println "enviarMensagem - $usuario - $mensagem"
+            OutputStream output = usuario.socket.outputStream
+            output << "$mensagem\n"
+            output.flush()
+        } catch(e) {
+            /*TODO implementar lógica do que fazer no caso de erro de mensagem ao usuário*/
+            println "falha na conexão do usuario $usuario"
+        }
 
+        // finally {
+        //    fecharConexao(usuario)
+        //}
+
+    }
+
+
+    void enviarMensagemReconecao(Usuario usuario) {
+        String msg = serialize([keepAlive: new Date().time])
+        enviarMensagem(usuario, msg)
+    }
+
+    void fecharConexao(Usuario usuario) {  
+        try{      
+            Socket socket = usuario.socket
+            if (socket && !socket.closed && socket.connected) {
+                String msg = serialize([close: new Date().time])
+                enviarMensagem(usuario, msg)            
+                socket.close()   
+            }
+        } catch (e) {
+            println "fecharConexao - conexão do $usuario já estava fechada"
+        }
+
+        usuarios.remove(usuario)
+          
+                 
+    }
+
+    private String serialize(Map m) {
+        // serialize
+        //m = [a: 123, b: 'test']
+        //JsonBuilder builder = new JsonBuilder()
+        //builder(m)
+        return m.inspect()
+        //println builder.toString()
+    }
+
+    private def deserialize(String msg) {
+        return Eval.me(msg)
     }
 
 
 
     void printStatus() {
+        println "************SERVER STATUS ****************"
         println "Usuarios conectados: ${usuarios}"
         println "Convitess: ${convites}"
-        println "Canais: ${canais}"
+        println "Partida: ${partidas}"
+        println "************SERVER STATUS ****************"
+
+        println '\n' * 4
     }
 
 
